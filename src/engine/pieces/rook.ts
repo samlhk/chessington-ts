80c6c3402cb64011b5cc44c6f3f85ddc7887018f
Sam Lee <Sam.Lee@softwire.com>
2023-06-23 11:22:14 +0100

    implemented red-4

--- src/engine/pieces/rook.ts
+++ src/engine/pieces/rook.ts
@@ -1,13 +1,31 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from '../square';
 
 export default class Rook extends Piece {
     public constructor(player: Player) {
         super(player);
     }
 
-    public getAvailableMoves(board: Board) {
-        return new Array(0);
+    public getAvailableMoves(board: Board): Square[] {
+
+        const currentSquare = board.findPiece(this);
+
+        const validMoves = [];
+
+        for (let i = 0; i < 8; i++) {
+            if (currentSquare.col !== i) {
+                validMoves.push(Square.at(currentSquare.row, i));
+            }
+        }
+
+        for (let i = 0; i < 8; i++) {
+            if (currentSquare.row !== i) {
+                validMoves.push(Square.at(i, currentSquare.col));
+            }
+        }
+
+        return validMoves;
     }
 }
