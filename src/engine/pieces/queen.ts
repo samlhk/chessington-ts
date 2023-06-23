581f8e3c6585d475d9103c0cc0672d2bcb2b2370
Sam Lee <Sam.Lee@softwire.com>
2023-06-23 11:42:07 +0100

    implemented red-6

--- src/engine/pieces/queen.ts
+++ src/engine/pieces/queen.ts
@@ -1,13 +1,40 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from '../square';
 
 export default class Queen extends Piece {
     public constructor(player: Player) {
         super(player);
     }
 
-    public getAvailableMoves(board: Board) {
-        return new Array(0);
+    public getAvailableMoves(board: Board): Square[] {
+        const currentSquare = board.findPiece(this);
+
+        const validMoves = [];
+
+        for (let i = 0; i < 8; i++) {
+            if (currentSquare.col !== i) {
+                validMoves.push(Square.at(currentSquare.row, i));
+            }
+        }
+
+        for (let i = 0; i < 8; i++) {
+            if (currentSquare.row !== i) {
+                validMoves.push(Square.at(i, currentSquare.col));
+            }
+        }
+
+        const dirs = [[1, 1], [1, -1], [-1, -1], [-1, 1]];
+        for (const [rowDir, colDir] of dirs) {
+            let [newRow, newCol] = [currentSquare.row + rowDir, currentSquare.col + colDir];
+            while (newRow >= 0 && newRow < 8 && newCol >= 0 && newCol < 8) {
+                validMoves.push(Square.at(newRow, newCol));
+                newRow += rowDir;
+                newCol += colDir;
+            }
+        }
+
+        return validMoves;
     }
 }
