6fc7287ac37ae5d906882b0540adf724351ffe74
Sam Lee <Sam.Lee@softwire.com>
2023-06-23 11:52:07 +0100

    implemented red-7

--- src/engine/pieces/knight.ts
+++ src/engine/pieces/knight.ts
@@ -1,13 +1,31 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from '../square';
 
 export default class Knight extends Piece {
     public constructor(player: Player) {
         super(player);
     }
 
-    public getAvailableMoves(board: Board) {
-        return new Array(0);
+    public getAvailableMoves(board: Board): Square[] {
+        const currentSquare = board.findPiece(this);
+        
+        const validMoves = [];
+
+        const dirs = [-2, -1, 1, 2];
+
+        for (const rowDir of dirs) {
+            for (const colDir of dirs) {
+                if (Math.abs(rowDir) !== Math.abs(colDir)) {
+                    let [newRow, newCol] = [currentSquare.row + rowDir, currentSquare.col + colDir];
+                    if (newRow >= 0 && newRow < 8 && newCol >= 0 && newCol < 8) {
+                        validMoves.push(Square.at(newRow, newCol));
+                    }
+                }
+            }
+        }
+
+        return validMoves;
     }
 }
